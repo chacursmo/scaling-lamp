b48b60036b2f8ac596b0eea67d8b877c6e4a914d
Sam Crouch <chacursmo@gmail.com>
2017-03-08 08:45:36 -0600

    FEATURE: More keys work

--- js/action.js
+++ js/action.js
@@ -6,12 +6,55 @@ $(document).ready(function(){
 var Controller = function(calc){
     $(".keyboard").click(function(data){
 	switch (data.target.id){
-	case "+":
+	case "plus":
 	    calc.screen.push("+");
 	    break;
 	case "eight":
 	    calc.screen.push("8");
 	    break;
+	case "nine":
+	    calc.screen.push("9");
+	    break;
+	case "seven":
+	    calc.screen.push("7");
+	    break;
+	case "six":
+	    calc.screen.push("6");
+	    break;
+	case "five":
+	    calc.screen.push("5");
+	    break;
+	case "four":
+	    calc.screen.push("4");
+	    break;
+	case "three":
+	    calc.screen.push("3");
+	    break;
+	case "two":
+	    calc.screen.push("2");
+	    break;
+	case "one":
+	    calc.screen.push("1");
+	    break;
+	case "zero":
+	    calc.screen.push("0");
+	    break;
+	case "minus":
+	    calc.screen.push("-");
+	    break;
+	case "times":
+	    calc.screen.push("*");
+	    break;
+	case "equals":
+	    calc.evaluate();
+	    break;
+	case "clear":
+	    calc.clear();
+	    break;
+	case "decimal":
+	    calc.screen.push(".");
+	    break;
+
 	default :
 	    console.log("Argwahl");
 	}
@@ -35,5 +78,11 @@ var Calculondo = function(){
 	$(".screen").html(this.screen);
 	console.log(this.screen);
 
+    }
+
+    this.clear = function(){
+	this.screen = [];
+    }
+    this.evaluate = function() {
     }
 }
