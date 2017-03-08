f8b9e67743c51a658a34ba3247816b6e85ed516b
Sam Crouch <chacursmo@gmail.com>
2017-03-08 07:29:40 -0600

    FEATURE: Daily Update

--- js/action.js
+++ js/action.js
@@ -1,8 +1,37 @@
 $(document).ready(function(){
-
-
-$(".seven").click(function(){
-    $(".screen").html("7");
+    var calc = new Calculondo();
+    var cont = new Controller(calc);
 });
 
-});
+var Controller = function(calc){
+    $(".keyboard").click(function(data){
+	switch (data.target.id){
+	case "+":
+	    calc.screen.push("+");
+	    break;
+	case "eight":
+	    calc.screen.push("8");
+	    break;
+	default :
+	    console.log("Argwahl");
+	}
+
+	calc.display();
+    });
+};
+
+var Calculondo = function(){
+    this.screen = [];
+    this.add = function(){
+	var sum = 0;
+	for (var i = 0; i < screen.length; i++){
+	    sum += screen[i];
+	}
+	return sum;
+    }
+
+    this.display = function(){
+	$(".screen").html(this.screen);
+	console.log(this.screen);
+    }
+}
